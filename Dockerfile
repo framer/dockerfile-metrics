dfc4aac08228a9c816c53fcf3d4a513dc5b3e07b
gdurandvadas <gdurandvadas@gmail.com>
2020-04-06 20:22:42 +0200

    use tail to keep container running

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM ubuntu:18.04
 
+COPY collect.sh /
 COPY crontab /etc/cron.d/collect-cron
 RUN chmod 0644 /etc/cron.d/collect-cron
 
 RUN apt-get update \
-  && apt-get -y install cron=3.0pl1-128.1ubuntu1 --no-install-recommends \
+  && apt-get -y install curl=7.58.0-2ubuntu3.8 cron=3.0pl1-128.1ubuntu1 awscli=1.14.44-1ubuntu1 --no-install-recommends \
   && apt-get clean \
   && rm -rf /var/lib/apt/lists/*
 
-CMD ["cron"]
+CMD ["cron", "&&", "tail", "-f", "/var/log/cron.log"]
--- collect.sh
+++ collect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-METRIC=$(curl -s http://localhost:${SERVICE_PORT}/metrics | grep ${METRIC_NAME})
+METRIC=$(curl -s http://localhost:${SERVICE_PORT}/metrics | grep ${METRIC_NAME} | awk {'print $2'})
  
-aws cloudwatch put-metric-data --metric-name ${METRIC_NAME} --dimensions ClusterName=${CLUSTER_NAME} --dimensions TaskDefinitionFamily=${CONTAINER_NAME} --namespace "ECS/Custom" --value ${METRIC}
+aws --region ${AWS_REGION} cloudwatch put-metric-data --metric-name ${METRIC_NAME} --dimensions ClusterName=${CLUSTER_NAME},TaskDefinitionFamily=${CONTAINER_NAME} --namespace "ECS/Custom" --value ${METRIC}
