17ab994a5eaf5c77a8e00037f46c815da5b36d10
gdurandvadas <gdurandvadas@gmail.com>
2020-04-07 07:57:18 +0200

    clean comments before getting the metric

--- collect.sh
+++ collect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-METRIC=$(curl -s http://${CONTAINER_NAME}:${SERVICE_PORT}/metrics | grep ${METRIC_NAME} | awk {'print $2'})
+METRIC=$(curl -s ${METRICS_ENDPOINT} | grep -v "#" | grep ${METRIC_NAME} | awk {'print $2'})
  
 aws cloudwatch put-metric-data --region ${AWS_REGION} --metric-name ${METRIC_NAME} --dimensions ClusterName=${CLUSTER_NAME},TaskDefinitionFamily=${CONTAINER_NAME} --namespace "ECS/Custom" --value ${METRIC}
