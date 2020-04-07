6a6bff4c11d49abf9520858928c28ad958b14a7d
gdurandvadas <gdurandvadas@gmail.com>
2020-04-07 07:16:43 +0200

    curl from app name

--- collect.sh
+++ collect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-METRIC=$(curl -s http://localhost:${SERVICE_PORT}/metrics | grep ${METRIC_NAME} | awk {'print $2'})
+METRIC=$(curl -s http://${CONTAINER_NAME}:${SERVICE_PORT}/metrics | grep ${METRIC_NAME} | awk {'print $2'})
  
-aws --region ${AWS_REGION} cloudwatch put-metric-data --metric-name ${METRIC_NAME} --dimensions ClusterName=${CLUSTER_NAME},TaskDefinitionFamily=${CONTAINER_NAME} --namespace "ECS/Custom" --value ${METRIC}
+aws cloudwatch put-metric-data --region ${AWS_REGION} --metric-name ${METRIC_NAME} --dimensions ClusterName=${CLUSTER_NAME},TaskDefinitionFamily=${CONTAINER_NAME} --namespace "ECS/Custom" --value ${METRIC}
